946a2955f9bbd9278e7c11f76cdc0578e78ce004
Mahadeb Sen <mahadebsen237@gmail.com>
2023-08-11 19:04:52 +0530

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../page/Home";
+import App from "../App";
+import Login from "../page/Login";
+import NotFound from "../page/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
